626b97c3e52b866853ed7cf1eba7733182043922
Daniel Drzimotta <daniel@drzimotta.com>
2011-12-24 15:58:58 -0800

    Made more consistent.

--- euler-1.lisp
+++ euler-1.lisp
@@ -1,3 +1,15 @@
+;; Problem 1
+
+;; If we list all the natural numbers below 10 that are multiples of 3 or 5, we 
+;; get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+;; Find the sum of all the multiples of 3 or 5 below 1000.
+
+;; Result should be 233168
+
+(defun euler-1 ()
+  (sum-of-multiples-of-3-or-5 1000))
+
 (defun multiple-of-3-or-5? (num-to-check)
   (if  (or (eq (mod num-to-check 3) 0) (eq (mod num-to-check 5) 0))
        num-to-check
