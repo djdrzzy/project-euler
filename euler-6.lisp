9fc5f50ae77abc878d723b367a57a31168de059b
Daniel Drzimotta <daniel@drzimotta.com>
2011-12-24 23:36:29 -0800

    Found a cool thing in the loop macro to sum instead of collecting and reducing the results.

--- euler-6.lisp
+++ euler-6.lisp
@@ -1,5 +1,4 @@
 ;; Problem 6
-
 ;; Find the difference between the sum of the squares of the first one hundred
 ;; natural numbers and the square of the sum.
 
@@ -9,8 +8,8 @@
   (- (square-of-sums 1 100) (sum-of-squares 1 100)))
 
 (defun sum-of-squares (s e)
-  (reduce #'+ (loop for i from s to e collect (* i i))))
+  (loop for i from s to e summing (* i i)))
 
 (defun square-of-sums (s e)
-  (let ((sum (reduce #'+ (loop for i from s to e collect i))))
+  (let ((sum (loop for i from s to e summing i)))
     (* sum sum)))
