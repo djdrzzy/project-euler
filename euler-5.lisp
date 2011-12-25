c4cae4aff9db16f937fbdb7341790811fec58529
Daniel Drzimotta <daniel@drzimotta.com>
2011-12-24 23:10:22 -0800

    Found a better way.

--- euler-5.lisp
+++ euler-5.lisp
@@ -10,9 +10,16 @@
 
 ;; Solution should be 232792560
 
+;; We could also use our FANCY NEW LISP MACRO
+(defmacro euler-5-lcm (s e)
+    `(lcm ,@(loop for i from s to e collect i)))
+
 (defun euler-5 ()
+  (euler-5-lcm 1 20))
+
+(defun euler-5-brute ()
   (loop for i from 20 by 20 do
        (let ((l (loop for j from 1 to 20
 		   collect (rem i j))))
 	 (when (zerop (length (remove-if #'zerop l)))
-	   (return-from euler-5 i)))))
+	   (return-from euler-5-brute i)))))
