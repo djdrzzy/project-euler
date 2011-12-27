dadba038525f9cc1fd5a067e76dc4bd146369beb
Daniel Drzimotta <daniel@drzimotta.com>
2011-12-27 02:21:24 -0800

    Big speedup! 1000000th prime in 34 seconds.

--- euler-7.lisp
+++ euler-7.lisp
@@ -7,24 +7,16 @@
 ;; Result should be 104743
 
 (defun euler-7 ()
-  (first (multiple-value-list (nth-prime 10001))))
+  (first (multiple-value-list (nth-prime 1000000))))
 
-
-;; We could turn that not any into a loop... and then up to the sqrt of the 
-;; n we are checking. That would cut our time down a lot...  
 (defun nth-prime (n)
-  (let ((set-of-primes (make-array n 
-				   :element-type 'fixnum 
-				   :initial-element 2))
+  (let ((set-of-primes (make-array n :element-type 'fixnum :initial-element 2))
 	(array-index 1)
 	(current-prime 2)
-	(num-to-check 3)
-	(slice-to-check nil))
+	(num-to-check 3))
     (loop
-       (setf slice-to-check (subseq set-of-primes 0 (round (sqrt array-index))))
-       (when (notany #'(lambda (p) 
-			 (zerop (rem num-to-check p))) 
-		     slice-to-check)
+       (when (loop for i from 0 to (round (sqrt array-index))
+		never (zerop (rem num-to-check (aref set-of-primes i))))
 	 (setf (aref set-of-primes array-index) num-to-check)
 	 (setf array-index (1+ array-index))
 	 (setf current-prime num-to-check))
